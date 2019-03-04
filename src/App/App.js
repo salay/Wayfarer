4beacfea262e977cd65b1a5b60bae92ed0c869ae
salay <siri.alay1@gmail.com>
2019-03-04 14:57:24 -0800

    file structure FINAL

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header'
-import Footer from './Footer'
-import Landing from './Landing/Landing';
-import Profile from './Profile/Profile';
-import Posts from './Posts/Posts';
+import Landing from '../Landing/Landing';
+import Profile from '../Profile/Profile';
+import Posts from '../Posts/Posts';
 import { Switch, Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
         <Switch>
             <Route path="/" exact strict component={Landing}/>
             <Route path="/profile"  exact strict component={Profile}/>
             <Route path="/posts"  exact strict component={Posts}/>
         </Switch>
-        <Footer />
       </div>
     );
   }
