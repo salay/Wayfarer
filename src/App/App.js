c5a1c91596583b08195f89d56f6ef0b20da956ed
salay <siri.alay1@gmail.com>
2019-03-04 14:42:45 -0800

    final folder structure plus background image added

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import Landing from './Landing/Landing';
-import Profile from './Profile/Profile';
-import Posts from './Posts/Posts';
-import { Switch, Route} from 'react-router-dom';
+import Landing from '../Landing/Landing';
+import Profile from '../Profile/Profile';
+import Posts from '../Posts/Posts';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+       Landing Page
         <Switch>
             <Route path="/" exact strict component={Landing}/>
             <Route path="/profile"  exact strict component={Profile}/>
             <Route path="/posts"  exact strict component={Posts}/>
         </Switch>
-        <Footer />
+     
       </div>
     );
   }
